23b31f0e261e4cff8cb7983de163a7b40dc9a17d
ollie@medea <lieshoff@gmx.de>
2025-12-27 01:30:31 +0100

    Clean up.

--- start-shell.sh
+++
@@ -1,7 +0,0 @@
-java \
--Dspring.datasource.url=jdbc:mariadb://localhost:3306/memnon \
--Dspring.datasource.driverClassName=org.mariadb.jdbc.Driver \
--Dspring.datasource.username=memnon \
--Dspring.datasource.password=password \
--Dlogging.level.root=WARN \
--jar shell/target/memnon-shell-0.0.1.jar
